6e58f7becc897f3c5ecc843e084a9b75e3e8ca46
niederle <claude.niederlender@inist.fr>
2017-06-21 11:12:00 +0200

    1ere version structuration des tests unitaires

--- test/run.js
+++ test/run.js
@@ -3,55 +3,58 @@
 'use strict';
 
 var
-  fs = require('fs')
+  fs = require('fs-extra')
+  , path = require('path')
   , pkg = require('../package.json')
   , business = require('../index.js')
-  , testData = require('./dataset/in/test.json')
   , chai = require('chai')
   , expect = chai.expect
   ;
 
+
+let jsonInput = {
+    ingest: {
+      type: 'zip',
+      path: path.join(__dirname, 'dataset/zip/input.zip')
+    },
+    corpusRoot: path.join(__dirname, 'dataset/zip/out')
+  };
+
 describe(pkg.name + '/index.js', function () {
-  describe('#doTheJob', function () {
 
-    it('docObject qui renvoie isConditor:true @1', function (done) {
+  // Méthde finale sensée faire du nettoyage après les tests
+  before(function (done) {
+
+    // Nettoyage du corpusRoot;
+    fs.mkdirsSync(jsonInput.corpusRoot);
+
+    done();
+
+  });
+
+  describe('#doTheJob pour les ingestions de type "zip"', function () {
+
+    it('devrait extraire les notices et générer les docObjects correspondant @1', function (done) {
       var docObject;
-      business.doTheJob(docObject = testData[0], function (err) {
-        expect(err).to.be.undefined;
-        expect(docObject.isConditor).to.be.true;
-        done();
-      });
-    });
-
-    it('docObject qui ne renvoie pas isConditor @2', function (done) {
-      var docObject;
-      business.doTheJob(docObject = testData[1], function (err) {
-        expect(err).to.be.not.undefined;
-        expect(err.code).to.equal(1);
-        expect(docObject.isConditor).to.be.false;
-        done();
-      });
-    });
-
-    it('docObject qui renvoie isConditor:true @3', function (done) {
-      var docObject;
-      business.doTheJob(docObject = testData[2], function (err) {
-        expect(err).to.be.undefined;
-        expect(docObject.isConditor).to.be.true;
-        done();
-      });
-    });
-
-    it('docObjects qui renvoie un docObject en erreur et modifie l\'autre @4', function (done) {
-      var docObjects = testData;
-      business.finalJob(docObjects, function (err) {
-        expect(err).to.be.not.undefined;
-        expect(err.length, 'err.length').to.equal(1);
-        expect(docObjects.length, 'docObjects.length').to.equal(2);
-        expect(docObjects[0].ending).to.equal('finalJob');
-        done();
-      });
+      // business.doTheJob(jsonInput, function (err) {
+      //   expect(err).to.be.undefined;
+      //   expect(docObject.isConditor).to.be.true;
+      //   done();
+      // });
+      done();
     });
 
   });
+
+  // Méthde finale sensée faire du nettoyage après les tests
+  after(function (done) {
+
+    // Nettoyage du corpusRoot;
+    fs.removeSync(jsonInput.corpusRoot);
+
+    done();
+
+  });
+
+
 });
