3597b24a92ff7d2de2e14240a68489432f52af5b
niederle <claude.niederlender@inist.fr>
2017-06-21 12:02:34 +0200

    ajout TU sur la présence des fichiers après unzip

--- test/run.js
+++ test/run.js
@@ -36,12 +36,28 @@ describe(pkg.name + '/index.js', function () {
 
     it('devrait extraire les notices et générer les docObjects correspondant @1', function (done) {
       var docObject;
-      // business.doTheJob(jsonInput, function (err) {
-      //   expect(err).to.be.undefined;
-      //   expect(docObject.isConditor).to.be.true;
-      //   done();
-      // });
-      done();
+      business.doTheJob(jsonInput, function (err) {
+
+        expect(err, "La fonction doTheJob ne devrait pas renvoyer d'erreur").to.be.undefined;
+
+        // vérifie que tous les fichiers du zip ont bien été dézippés
+        const unzippedFiles = [
+          path.join(jsonInput.corpusRoot,'notice1.xml'),
+          path.join(jsonInput.corpusRoot,'notice2.xml'),
+          path.join(jsonInput.corpusRoot,'1/notice1-1.xml'),
+          path.join(jsonInput.corpusRoot,'2/notice2-1.xml'),
+          path.join(jsonInput.corpusRoot,'2/1/notice2-1-1.xml'),
+          path.join(jsonInput.corpusRoot,'2/1/notice2-1-2.xml'),
+          path.join(jsonInput.corpusRoot,'2/1/notice2-1-3.xml'),
+          path.join(jsonInput.corpusRoot,'2/2/notice2-2-1.xml'),
+          path.join(jsonInput.corpusRoot,'2/2/notice2-2-2.txt'),
+          path.join(jsonInput.corpusRoot,'2/2/notice2-2-3.xml')
+        ];
+        for (let f of unzippedFiles) {
+          expect(fs.existsSync(f),`Le fichier ${f} devrait exister`).to.be.true;
+        }
+        done();
+      });
     });
 
   });
